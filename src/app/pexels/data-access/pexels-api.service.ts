8280803f47ec739d8d78f8381c6ab5548402700a
DevWedeloper <vicnathangabrielle@gmail.com>
2024-02-06 16:48:10 +0800

    feat: add pexels api

---
+++ src/app/pexels/data-access/pexels-api.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { PexelsSearchPhotosData } from '../types/pexels.type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PexelsApiService {
+  private http = inject(HttpClient);
+  private url = environment.searchPhotosUrl;
+
+  searchPhotos(
+    query: string,
+    perPage: number,
+  ): Observable<HttpResponse<PexelsSearchPhotosData>> {
+    return this.http.get<PexelsSearchPhotosData>(
+      `${this.url}?query=${query}&per_page${perPage}`,
+      { observe: 'response' },
+    );
+  }
+}
