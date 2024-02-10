5b4a5f748ad47f881b0f561e2015f6a9b22f1029
DevWedeloper <vicnathangabrielle@gmail.com>
2024-02-10 17:04:05 +0800

    feat: add mat-toolbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { Store } from '@ngrx/store';
 import { Subject, debounceTime, distinctUntilChanged, take } from 'rxjs';
 import { pexelsActions } from '../pexels/state/pexels.actions';
@@ -37,6 +38,7 @@ import { ThemeService } from '../shared/data-access/theme.service';
     MatButtonModule,
     MatIconModule,
     ScrollingModule,
+    MatToolbarModule,
     CardComponent,
   ],
   templateUrl: './home.component.html',
